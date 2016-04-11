ff07c0e1f23375a6e2f544fb12c142f468ffb97a
Dingyuan Wang <abcdoyle888@gmail.com>
2016-04-11 20:36:26 +0800

    complete simpdump

--- simpdump.py
+++ simpdump.py
@@ -3,105 +3,139 @@
 
 import re
 import sys
+import hashlib
 import collections
 import multiprocessing
 
 '''
 This script tries to fvck these various esoteric hard-to-process
 user database dump or leak files.
+
+Format: username <TAB> email <TAB> password <TAB> md5
 '''
 
-#Fields = collections.namedtuple('Fields', ('username', 'email', 'password', 'md5'))
+md5hash = lambda s: hashlib.md5(s.encode('utf-8')).hexdigest()
 
 seperators = ('----', '\t|\t', '\t', '|', ',', None)
 
-templates = (
-('password',),
-('email', 'password'),
-('username', 'password'),
-('username', 'password', 'email'),
-('username', 'md5', 'email'),
-('username', 'md5', 'password'),
-('username', 'md5', 'email', 'password'),
-('username', 'email', 'md5', 'password'),
-)
+#templates = (
+#('password',),
+#('email', 'password'),
+#('username', 'password'),
+#('username', 'password', 'email'),
+#('username', 'md5', 'email'),
+#('username', 'md5', 'password'),
+#('username', 'md5', 'email', 'password'),
+#('username', 'email', 'md5', 'password'),
+#)
 
-templates_index = collections.defaultdict(list)
-for t in templates:
-    templates_index[len(t)].append(t)
+#templates_index = collections.defaultdict(list)
+#for t in templates:
+    #templates_index[len(t)].append(t)
 
-re_ctrl = re.compile("[\000-\037]+")
+re_ctrl = re.compile("[\x00-\x08\x0A-\x1F]+")
 remove_str = (
-    "用户组&emsp;:<span class='gray'>平民 (-1)",
-    "<tr><td>用户IP&emsp;:<span class=gray>"
+    u"用户组&emsp;:<span class='gray'>平民 (-1)",
+    u"<tr><td>用户IP&emsp;:<span class=gray>"
 )
+#invalid_username = frozenset((u"用户组&emsp;:<span class='gray'>平民 (-1) ",))
+
+re_whitespace = re.compile('[\t ]+')
+re_password = re.compile('^[ -~]{6,}$')
+re_username_ext = re.compile(r'^\S{2,}$', re.U)
 
 re_formats = collections.OrderedDict((
-    ('email', re.compile('^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+$')),
+    ('email', re.compile('^[A-Za-z0-9._-]+@[A-Za-z0-9-]+\.[A-Za-z0-9.-]{2,}$')),
     ('md5', re.compile('^[A-Fa-f0-9]{32}$')),
-    ('username', re.compile(r'^[\w.]+$')),
-    ('password', re.compile('^[ -~]+$')),
-    ('username_ext', re.compile(r"^\S+$", re.U))
+    ('username', re.compile(r'^[\w.]{2,}$')),
+    ('password', re_password),
+    #('username_ext', re.compile(r"^\S+$", re.U))
+    (None, None) # catch all username/nickname
 ))
 
 def sanitize(s):
     s = re_ctrl.sub('', s.strip())
+    try:
+        s = s.decode('utf-8')
+    except UnicodeDecodeError:
+        s = s.decode('gbk', 'ignore')
     for rm in remove_str:
         s = s.replace(rm, '')
-    try:
-        return s.decode('utf-8')
-    except UnicodeDecodeError:
-        return s.decode('gbk', 'ignore')
+    return s
 
 def try_formats(s):
-    print(s.encode('utf-8'))
+    if not s:
+        return
+    #print(s.encode('utf-8'))
     for sep in seperators:
-        fields = tuple(filter(None, s.split(sep)))
+        if sep:
+            fields = filter(None, s.split(sep))
+        else:
+            fields = filter(None, re_whitespace.split(s))
+        #print(sep, fields)
         if len(fields) < 2:
             continue
-        print(fields)
-        try:
-            less_match = None
-            for tmpl in templates_index[len(fields)]:
-                fields_match = ['']*4
-                for k, f in enumerate(fields):
-                    if re_formats[tmpl[k]].match(f):
-                        fields_match[k] = f
-                if all(fields_match):
-                    return dict(zip(tmpl, fields_match))
-                elif fields_match:
-                    if (not less_match or
-                        sum(map(bool, fields_match)) > sum(map(bool, less_match))):
-                        less_match = fields_match
-            else:
-                if less_match:
-                    return dict(zip(tmpl, less_match))
-        except KeyError:
+        for f in fields:
             fields_got = {}
-            flist = list(re_formats.items())
+            flist = re_formats.copy()
             for f in fields:
-                for k, (ftype, regex) in enumerate(flist):
-                    if regex.match(f):
-                        fields_got[ftype] = f
+                for ftype, regex in flist.items():
+                    if ftype:
+                        if regex.match(f):
+                            fields_got[ftype] = f
+                            break
+                    elif sep:
+                        fields_got['username'] = f
+                        break
+                    elif re_username_ext.match(f):
+                        fields_got['username'] = f
                         break
                 else:
                     continue
-                del flist[k]
+                del flist[ftype]
+                if ftype is None:
+                    try:
+                        del flist['username']
+                    except KeyError:
+                        pass
+                elif ftype == 'username':
+                    try:
+                        del flist[None]
+                    except KeyError:
+                        pass
             return fields_got
     else:
-        if re_formats['password'].match(s):
+        if re_password.match(s):
             return {'password': s}
+
+def process_result(r):
+    password = r.get('password', '')
+    md5 = r.get('md5', '').lower()
+    username = r.get('username', '')
+    email = r.get('email', '').lstrip('-')
+    if password and md5:
+        if md5hash(password) == md5:
+            md5 = ''
+        else:
+            password = ''
+    elif username and not password and re_password.match(username):
+        password = username
+        username = ''
+    #elif username in invalid_username:
+        #username = ''
+    return (username, email, password, md5)
 
 def process_line(l):
     r = try_formats(sanitize(l))
-    print(r)
+    #print(r)
     if r:
-        print('\t'.join((r.get('username', ''), r.get('email', ''),
-              r.get('password', ''), r.get('md5', ''))).encode('utf-8'))
+        print('\t'.join(process_result(r)).encode('utf-8'))
 
 if __name__ == '__main__':
-    #pool = multiprocessing.Pool(1)
-    #func = pool.imap_unordered
-    func = map
+    if len(sys.argv) > 1 and sys.argv[1] == '-1':
+        func = map
+    else:
+        pool = multiprocessing.Pool(1)
+        func = pool.imap_unordered    
     for _ in func(process_line, sys.stdin):
         pass
