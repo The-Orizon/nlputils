046e01331087f8151e5157338504335e801b1ee2
Dingyuan Wang <abcdoyle888@gmail.com>
2016-04-14 22:04:52 +0800

    clean code

--- simpdump.py
+++ simpdump.py
@@ -18,27 +18,11 @@ md5hash = lambda s: hashlib.md5(s.encode('utf-8')).hexdigest()
 
 seperators = ('\t', '----', '|', ',', None)
 
-#templates = (
-#('password',),
-#('email', 'password'),
-#('username', 'password'),
-#('username', 'password', 'email'),
-#('username', 'md5', 'email'),
-#('username', 'md5', 'password'),
-#('username', 'md5', 'email', 'password'),
-#('username', 'email', 'md5', 'password'),
-#)
-
-#templates_index = collections.defaultdict(list)
-#for t in templates:
-    #templates_index[len(t)].append(t)
-
 re_ctrl = re.compile("[\x00-\x08\x0A-\x1F]+")
 remove_str = (
     u"用户组&emsp;:<span class='gray'>平民 (-1)",
     u"<tr><td>用户IP&emsp;:<span class=gray>"
 )
-#invalid_username = frozenset((u"用户组&emsp;:<span class='gray'>平民 (-1) ",))
 
 re_whitespace = re.compile('[\t ]+')
 re_password = re.compile('^[ -~]{6,64}$')
@@ -65,13 +49,11 @@ def sanitize(s):
 def try_formats(s):
     if not s:
         return
-    #print(s.encode('utf-8'))
     for sep in seperators:
         if sep:
             fields = filter(None, s.split(sep))
         else:
             fields = filter(None, re_whitespace.split(s))
-        #print(sep, fields)
         if len(fields) < 2:
             continue
         for f in fields:
@@ -119,13 +101,10 @@ def process_result(r):
     elif username and not password and re_password.match(username):
         password = username
         username = ''
-    #elif username in invalid_username:
-        #username = ''
     return (username, email, password, md5)
 
 def process_line(l):
     r = try_formats(sanitize(l))
-    #print(r)
     if r:
         return u'\t'.join(process_result(r)).encode('utf-8')
 
