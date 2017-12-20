95dc42f8806ae6d6a03b0d63040c133d9997b7f6
Dingyuan Wang <abcdoyle888@gmail.com>
2017-12-20 17:23:56 +0800

    haodoo: update by date

--- haodoo/getpdblink.py
+++ haodoo/getpdblink.py
@@ -7,14 +7,22 @@ import itertools
 
 preference = {v:k for k,v in enumerate(('updb', 'pdb', 'epub', 'mobi', 'vepub', 'prc'))}
 
-def main(filename):
+def main(filename, update=None):
     db = sqlite3.connect(filename)
     cur = db.cursor()
     for bookid, group in itertools.groupby(cur.execute(
-        'SELECT * FROM files ORDER BY bookid, type'),
+        "SELECT * FROM files "
+        "WHERE julianday(coalesce(update_date, add_date, '9999-01-01')) "
+        " >= julianday(?) ORDER BY bookid, type", (update or '0001-01-01',)),
         key=lambda x: x[2]):
         files = sorted(group, key=lambda x: preference.get(x[1], 100))
         print('http://www.haodoo.net/?M=d&P=' + files[0][0])
 
 if __name__ == '__main__':
-    main(sys.argv[1] if len(sys.argv) > 1 else 'haodoo.db')
+    filename = 'haodoo.db'
+    update = None
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    if len(sys.argv) > 2:
+        update = sys.argv[2]
+    main(filename, update)
